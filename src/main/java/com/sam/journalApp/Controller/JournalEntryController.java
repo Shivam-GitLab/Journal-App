11756ff4191e51dd0a127f176e1cac52c8eb802f
Shivam-GitLab <shivam_223033@saitm.org>
2025-06-29 23:14:29 +0530

    Code

--- src/main/java/com/sam/journalApp/Controller/JournalEntryController.java
+++ src/main/java/com/sam/journalApp/Controller/JournalEntryController.java
@@ -17,7 +17,7 @@ public class JournalEntryController {
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
 //        return new ArrayList<>(journalEntries.values());
-//        return new ArrayList<>(journalEntries.values());
+
 
     }
 
