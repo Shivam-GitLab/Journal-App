5fc3165ab5079ef4dc5a68e276289919079ee103
Shivam-GitLab <shivam_223033@saitm.org>
2025-12-04 23:58:58 +0530

    User Repo Update

--- src/main/java/com/sm/journalApp/repositories/UserRepository.java
+++ src/main/java/com/sm/journalApp/repositories/UserRepository.java
@@ -5,8 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
-@Repository // Optional, but a good practice
+@Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByUserName(String userName);
     void deleteByUserName(String name);
